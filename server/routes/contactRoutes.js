9192c93cf5af8bcbdc74c22a32a8ecf683e8c94b
yugpatel9597 <131437656+yugpatel9597@users.noreply.github.com>
2026-03-08 13:39:38 +0530

    Delete server directory

--- server/routes/contactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendContactEmail } = require('../controllers/contactController');
-
-router.post('/', sendContactEmail);
-
-module.exports = router;
--- server/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createRazorpayOrder, verifyPayment } = require('../controllers/paymentController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/create-order', protect, createRazorpayOrder);
-router.post('/verify', protect, verifyPayment);
-
-module.exports = router;
